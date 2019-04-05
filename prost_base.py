a0701a5c484e3ae631c9e9ab7cbdc87bdb73aceb
Devon Yates <devonyates11@gmail.com>
2019-04-05 16:23:41 -0700

    Rename file

--- prost_base.py
+++ prost_base.py
@@ -1,12 +1,6 @@
 import abc
 
-class SparqlOverSpark(abc.ABC):
-
-	# Any shared data strcutures or methods should be defined as part of the parent class.
-	
-	# A list of shared arguments should be defined for each of the following methods and replace (or precede) *args.
-	
-	# The output of each of the following methods should be defined clearly and shared between all methods implemented by members of the group. 
+class ProstBase(abc.ABC):
 
     @classmethod
     @abc.abstractmethod
